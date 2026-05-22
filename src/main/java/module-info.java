7b387133d8d9f1a03d70783508da8788a63bb6c6
Gabbo <g.pannuto@studenti.unisa.it>
2026-05-22 15:35:27 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.progetto_sad {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.progetto_sad to javafx.fxml;
+    exports com.example.progetto_sad;
+}

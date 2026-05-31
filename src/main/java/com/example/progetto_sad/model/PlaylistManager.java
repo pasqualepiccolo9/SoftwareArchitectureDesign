8716776cdf89fb8c853369b8c22de957b5be41ef
mich-sant <m.santoro98@studenti.unisa.it>
2026-05-31 11:43:02 +0200

    Creato modello Playlist e PlaylistManager

--- src/main/java/com/example/progetto_sad/model/PlaylistManager.java
+++ src/main/java/com/example/progetto_sad/model/PlaylistManager.java
@@ -6,20 +6,19 @@ import java.util.List;
 
 public class PlaylistManager {
 
-    private final List<Playlist> playlists = new ArrayList<>();
+    private final List<Playlist> playlists;
+    
+    public PlaylistManager() {
+        this.playlists = new ArrayList<>();
+    }
 
     public Playlist createPlaylist(String name) {
-        // TODO US5: validare il nome (non nullo/non vuoto) prima di creare la playlist.
         Playlist playlist = new Playlist(name);
         playlists.add(playlist);
         return playlist;
     }
 
-    public void removePlaylist(Playlist p) {
-        playlists.remove(p);
-    }
-
     public List<Playlist> getPlaylists() {
-        return Collections.unmodifiableList(new ArrayList<>(playlists));
+        return new ArrayList<>(playlists);
     }
 }
